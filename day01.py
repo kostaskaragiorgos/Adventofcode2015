e84a37a56763d4a5e58cde662108b84f7fda72f7
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-06-27 14:45:57 +0300

    Update day01.py

--- day01.py
+++ day01.py
@@ -10,8 +10,21 @@ def findfloor(s):
             floor -= 1
     return floor
 
+def findbs(s):
+    floor = 0
+    counter = 0
+    for i in s:
+        if i == "(":
+            floor +=1
+        else:
+            floor -= 1
+        counter +=1
+        if floor == -1:
+            return counter
+
 with open("day1.txt") as f:
     lines = f.read()
     lines = lines.strip("\n")
     print(findfloor(lines))
+    print(findbs(lines))
         
