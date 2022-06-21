94596547ad33bf5e5ae73e1d9131d89920cacb00
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-21 16:24:45 +0300

    Update day06.py

--- day06.py
+++ day06.py
@@ -1,4 +1,12 @@
 import re
+import numpy as np
+
+def turnon(arr, ax, ay, bx, by):
+    for i in range(ax, bx+1):
+        for j in range(ay, by+1):
+            arr[i][j] = 1
+    return arr
+
 with open("day06.txt") as f:
     lines = f.read()
     lines = lines.split("\n")
